1db73d3b1d0a66e954a734243f38d2f34e8fde34
Marcus.J <airsize@gmail.com>
2019-09-17 20:51:07 +1200

    - Move target code from temporary folder to formal folder - Remove temporary test code - Bring back front end

--- Assignment_2 G2/DrawerJack.py
+++ Assignment_2 G2/DrawerJack.py
@@ -4,13 +4,13 @@ from TIGr import AbstractDrawer
 
 
 class Drawer(AbstractDrawer):
-    # config = open('config.txt', "r+").read().splitlines()
-    # if config[2] == 'FrontEndKieran':
-    #     from FrontEndKieran import TkinterInterface
-    #     this_canvas = TkinterInterface.canvas
-    # elif config[2] == 'FrontEndJerry':
-    #     from FrontEndJerry import GuiInterface
-    #     this_canvas = GuiInterface.canvas
+    config = open('config.txt', "r+").read().splitlines()
+    if config[2] == 'FrontEndKieran':
+        from FrontEndKieran import TkinterInterface
+        this_canvas = TkinterInterface.canvas
+    elif config[2] == 'FrontEndJerry':
+        from FrontEndJerry import GuiInterface
+        this_canvas = GuiInterface.canvas
 
     def __init__(self):
         self.colour = ''
--- Assignment_2 G2/DrawerKieran.py
+++ Assignment_2 G2/DrawerKieran.py
@@ -8,14 +8,13 @@ from TIGr import AbstractDrawer
 class Drawer(AbstractDrawer):
     x_pos = 0
     y_pos = 0
-
-    # config = open('config.txt', "r+").read().splitlines()
-    # if config[2] == 'FrontEndKieran':
-    #     from FrontEndKieran import TkinterInterface
-    #     this_canvas = TkinterInterface.canvas
-    # elif config[2] == 'FrontEndJerry':
-    #     from FrontEndJerry import GuiInterface
-    #     this_canvas = GuiInterface.canvas
+    config = open('config.txt', "r+").read().splitlines()
+    if config[2] == 'FrontEndKieran':
+        from FrontEndKieran import TkinterInterface
+        this_canvas = TkinterInterface.canvas
+    elif config[2] == 'FrontEndJerry':
+        from FrontEndJerry import GuiInterface
+        this_canvas = GuiInterface.canvas
 
     def __init__(self):
         self.test_string = ''
